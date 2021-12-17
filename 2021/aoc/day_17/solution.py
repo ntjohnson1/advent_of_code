f3874100a65a5446c5658aee6f6e69ed75c6902e
nickj <nickj@stanford.edu>
2021-12-17 15:57:55 -0500

    Day 17 part 2 as a second function to avoid re-write

--- 2021/aoc/day_17/solution.py
+++ 2021/aoc/day_17/solution.py
@@ -9,6 +9,8 @@ from itertools import product
 # p_t = p_0 + v_yt - 1/2(t-1)*t
 # peak height is
 # when v_y = t-1
+# reach p_t==p_0 when
+# t=2v_y+1
 
 def find_min_t(min_x, max_x):
     """
@@ -40,7 +42,7 @@ def find_final_positions_x(max_x):
 def find_final_positions_y_negative(min_y):
     """
     Returns matrix of shape (time_steps, integer_velocities)
-    Time steps are relative to when y_Vel becomes negative
+    Time steps are relative to 0
     """
     # We know we throw up, so at height_0 our velocity is flipped
     # find which speeds thrown straight down land in the region
@@ -49,6 +51,23 @@ def find_final_positions_y_negative(min_y):
     # Here we treat our velocities as candidate times as well (introduces need for negative)
     final_positions = -np.outer(candidate_y_velocities, candidate_y_velocities) - 0.5 * (
                 candidate_y_velocities * (candidate_y_velocities + 1))
+    return final_positions
+
+
+def find_final_positions_y_positive(max_y_velocity, max_time):
+    """
+    Technically non-negative
+    Returns matrix of shape (time_steps, integer_velocities)
+    Time steps are relative to 0
+    """
+    # We know we throw up, so at height_0 our velocity is flipped
+    # find which speeds thrown straight down land in the region
+    # anything faster than min_t when negative will be past the region
+    candidate_y_velocities = np.arange(max_y_velocity+1)
+    candidate_times = np.arange(1, max_time)
+    # Here we treat our velocities as candidate times as well (introduces need for negative)
+    final_positions = np.outer(candidate_y_velocities, candidate_times) - 0.5 * (
+                candidate_times * (candidate_times - 1))
     return final_positions
 
 
@@ -75,22 +94,38 @@ def problem_2(inputs):
     fy = find_final_positions_y_negative(min_y)
     valid_x = (fx <= max_x) & (fx >= min_x)
     valid_y = (fy <= max_y) & (fy >= min_y)
-    total_valid = 0
+
+    # Need to do non-negative initial velocities separately
+    # because I didn't want to do a full re-write from part 1
+    max_y_vel = np.max(np.where(valid_y)[0])
+    max_t = 2*max_y_vel + 1 + len(fy)
+    fy_pos = find_final_positions_y_positive(max_y_vel, max_t)
+    valid_y_pos = (fy_pos <= max_y) & (fy_pos >= min_y)
+
+    # Need to track pairs since some velocities yield
+    # valid positions at multiple time stamps
     valid_pairs = []
     for i in range(min(len(fx), len(fy))):
         # For negative initial y
-        total_valid += np.sum(valid_x[:, i]) * np.sum(valid_y[:, i])
         if np.any(valid_x[:, i]) and np.any(valid_y[:, i]):
             x_velocities = np.where(valid_x[:, i])[0]+1
             y_velocities = -(np.where(valid_y[:, i])[0]+1)
             valid_pairs.extend(list(product(x_velocities, y_velocities)))
 
-    return (len(set(valid_pairs)))
+    for i in range(min(len(fx), fy_pos.shape[1])):
+        if np.any(valid_x[:, i]) and np.any(valid_y_pos[:, i]):
+            x_velocities = np.where(valid_x[:, i])[0]+1
+            y_velocities = (np.where(valid_y_pos[:, i])[0])
+            valid_pairs.extend(list(product(x_velocities, y_velocities)))
 
-if __name__=="__main__":
+    return len(set(valid_pairs))
+
+
+if __name__ == "__main__":
     # Input is short don't bother parsing
-    #real_input = [(235, 259), (-118, -62)]
-    real_input = [(20, 30), (-10, -5)]
+    real_input = [(235, 259), (-118, -62)]
 
-    print(problem_1(real_input))
-    print(problem_2(real_input))
+    with solution_timing("Problem 1"):
+        print(problem_1(real_input))
+    with solution_timing("Problem 2"):
+        print(problem_2(real_input))
